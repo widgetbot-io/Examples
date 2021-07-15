dc5d46a206881166048584e18e8cb8bc277838de
wildflowericecoffee <36643731+wildflowericecoffee@users.noreply.github.com>
2021-07-15 17:33:29 -0500

    Update _app.tsx

--- nextjs/pages/_app.tsx
+++ nextjs/pages/_app.tsx
@@ -6,30 +6,3 @@ const App = ({ Component, pageProps }: AppProps) => {
 };
 
 export default App;
-
-
-// const WidgetBot = dynamic(() => import("@widgetbot/react-embed"), {
-//   ssr: false,
-// });
-
-// useEffect(() => {
-//   async function loadCrate() {
-//     const result = await import("@widgetbot/crate");
-//     const Crate = await result.cdn();
-
-//     new Crate({
-//       server: "299881420891881473",
-//       channel: "355719584830980096",
-//     });
-//   }
-
-//   loadCrate();
-// }, []);
-
-{/* <section className="landing-bg w-screen max-w-full h-screen-90 flex items-center justify-center h-screen">
-  <WidgetBot
-    server="299881420891881473"
-    channel="355719584830980096"
-    className="w-2/3 h-2/3"
-  />
-</section> */}
